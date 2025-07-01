14ea4f407ae95125c1e3e432cc1764f1ed473586
Muhammed <muhammedsosun06@gmail.com>
2025-07-01 19:37:10 +0300

    Accountmapper ile AccountService kısmı update edildi

---
+++ src/main/java/com/sosunmuhammed/gallerist/mapper/AccountMapper.java
@@ -0,0 +1,35 @@
+package com.sosunmuhammed.gallerist.mapper;
+
+import com.sosunmuhammed.gallerist.dto.DtoAccount;
+import com.sosunmuhammed.gallerist.dto.DtoAccountIU;
+import com.sosunmuhammed.gallerist.model.Account;
+
+import java.util.Date;
+
+public class AccountMapper {
+    public static Account toEntity(DtoAccountIU dto){
+        Account account = new Account();
+        account.setAccountNo(dto.getAccountNo());
+        account.setIban(dto.getIban());
+        account.setAmount(dto.getAmount());
+        account.setCurrencyType(dto.getCurrencyType());
+        account.setCreateTime(new Date());
+        return account;
+    }
+    public static void updateEntity(Account account,DtoAccountIU dto){
+        account.setAccountNo(dto.getAccountNo());
+        account.setIban(dto.getIban());
+        account.setAmount(dto.getAmount());
+        account.setCurrencyType(dto.getCurrencyType());
+    }
+    public static DtoAccount toDto(Account account){
+        DtoAccount dto = new DtoAccount();
+        dto.setId(account.getId());
+        dto.setAccountNo(account.getAccountNo());
+        dto.setIban(account.getIban());
+        dto.setAmount(account.getAmount());
+        dto.setCurrencyType(account.getCurrencyType());
+        dto.setCurrencyType(account.getCurrencyType());
+        return dto;
+    }
+}
--- src/main/java/com/sosunmuhammed/gallerist/service/impl/AccountServiceImpl.java
+++ src/main/java/com/sosunmuhammed/gallerist/service/impl/AccountServiceImpl.java
@@ -2,17 +2,15 @@ package com.sosunmuhammed.gallerist.service.impl;
 
 import com.sosunmuhammed.gallerist.dto.DtoAccount;
 import com.sosunmuhammed.gallerist.dto.DtoAccountIU;
-import com.sosunmuhammed.gallerist.dto.DtoAddress;
-import com.sosunmuhammed.gallerist.dto.DtoAddressIU;
 import com.sosunmuhammed.gallerist.exception.BaseException;
 import com.sosunmuhammed.gallerist.exception.ErrorMessage;
 import com.sosunmuhammed.gallerist.exception.MessageType;
+import com.sosunmuhammed.gallerist.mapper.AccountMapper;
 import com.sosunmuhammed.gallerist.model.Account;
 import com.sosunmuhammed.gallerist.repository.AccountRepository;
 import com.sosunmuhammed.gallerist.service.IAccountService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -22,42 +20,33 @@ import java.util.stream.Collectors;
 @Service
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired
-    private AccountRepository accountRepository;
 
-    private Account createAccount(DtoAccountIU dtoAccountIU){
-        Account account = new Account();
-        account.setCreateTime(new Date());
-        BeanUtils.copyProperties(dtoAccountIU,account);
-        return account;
+    private final AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
     }
+
 
     @Override
     public DtoAccount save(DtoAccountIU dtoAccountIU) {
-        DtoAccount dtoAccount = new DtoAccount();
-        Account savedAccount = accountRepository.save(createAccount(dtoAccountIU));
-        BeanUtils.copyProperties(savedAccount,dtoAccount);
-        return dtoAccount;
+        Account savedAccount = accountRepository.save(AccountMapper.toEntity(dtoAccountIU));
+        return AccountMapper.toDto(savedAccount);
     }
 
     @Override
     public List<DtoAccount> getAll() {
-        List<Account> accounts = accountRepository.findAll();
 
-        return accounts.stream().map(account ->{
-            DtoAccount dtoAccount = new DtoAccount();
-            BeanUtils.copyProperties(account,dtoAccount);
-            return dtoAccount;
-        }).collect(Collectors.toList());
+        return accountRepository.findAll().stream()
+                .map(AccountMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public DtoAccount findAccount(Long id) {
         Account account = accountRepository.findById(id)
-                .orElseThrow(()->new BaseException(new ErrorMessage(MessageType.NO_RECORD_EXIST,id.toString())));
-        DtoAccount dtoAccount = new DtoAccount();
-        BeanUtils.copyProperties(account,dtoAccount);
-        return dtoAccount;
+                .orElseThrow(() ->new BaseException(new ErrorMessage(MessageType.NO_RECORD_EXIST,id.toString())));
+        return AccountMapper.toDto(account);
     }
 
     @Override
@@ -71,15 +60,8 @@ public class AccountServiceImpl implements IAccountService {
     public DtoAccount update(Long id,DtoAccountIU dtoAccountIU) {
         Account account = accountRepository.findById(id).
                 orElseThrow(() ->new BaseException(new ErrorMessage(MessageType.NO_RECORD_EXIST,id.toString())));
-        account.setAccountNo(dtoAccountIU.getAccountNo());
-        account.setIban(dtoAccountIU.getIban());
-        account.setAmount(dtoAccountIU.getAmount());
-        account.setCurrencyType(dtoAccountIU.getCurrencyType());
-
-        Account saveAccount = accountRepository.save(account);
-
-        DtoAccount dtoAccount = new DtoAccount();
-        BeanUtils.copyProperties(saveAccount,dtoAccount);
-        return dtoAccount;
+        AccountMapper.updateEntity(account,dtoAccountIU);
+        Account updated = accountRepository.save(account);
+        return AccountMapper.toDto(updated);
     }
 }
